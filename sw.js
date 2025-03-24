a254e2e424798c6a8e4d369ca96e65d6e3b99698
227735 <monika.k24@onet.pl>
2025-03-24 14:29:18 +0100

    changes

--- sw.js
+++ sw.js
@@ -1,20 +1,23 @@
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open('pwa-cache').then(cache => {
-            return cache.addAll([
-                'index.html',
-                'map.html',
-                'manifest.json',
-                'sw.js'
-            ]);
-        })
-    );
+const CACHE_NAME = "pwa-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./main.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
